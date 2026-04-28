9415962a30b33cf4142c82158ff83eaba3c13a32
Axeljonta <axeljonta@gmail.com>
2026-04-28 02:05:41 -0300

    Finalizacion de la barra de navegación, se agregó un ícono a "Crear cuenta" y se mejoró el diseño del mismo. Además, se importó la biblioteca de iconos "react-icons" para utilizar el ícono de doble flecha.

--- frontEnd/EnRenta-Proyect/src/components/Navbar/Navbar.jsx
+++ frontEnd/EnRenta-Proyect/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import logo from '../../assets/LogoNegro.png'
 import './Navbar.css'
+import { FaAngleDoubleRight } from "react-icons/fa";
+import { HiMenuAlt2 } from "react-icons/hi";
 
 function Navbar() {
   
@@ -7,11 +9,24 @@ function Navbar() {
   
     return (
         <nav className="navbar">
-        <div className="navbar-logo"><img src={logo} alt="Logo" /></div>
+        
+        <div className='menu'>
+            <HiMenuAlt2 className='menu-icon'/>
+        </div>
+
+        <div className="navbar-logo">
+            <img src={logo} alt="Logo" />
+        </div>
+        
         <ul className="navbar-links">
-            <li><a href="/">Crear cuenta</a></li>
-            <li><a href="/about">Iniciar sesion</a></li>
+            <li>
+                <a href="/"> <FaAngleDoubleRight /> Crear cuenta</a>
+            </li>
+            <li>
+                <a href="/about">Iniciar sesion</a>
+            </li>
         </ul>
+        
         </nav>
     );
 }
